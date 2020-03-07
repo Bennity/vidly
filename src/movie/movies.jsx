cfaae0bb91e55c4c879975171a70bb204a63b385
bennity <b.derksen@mail.de>
2020-03-07 15:18:26 +0100

    pagination function

--- src/movie/movies.jsx
+++ src/movie/movies.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { getMovies } from "./../services/fakeMovieService";
 import Movie from "./movie";
+import Pagination from "../pagination/pagination";
+import movie from "./movie";
 
 class movies extends Component {
   fetchMovies = () => {
@@ -10,12 +12,34 @@ class movies extends Component {
         Object.assign(obj, { liked: "fa fa-heart-o" })
       );
     });
+    this.calcPages(moviewithliked);
     return moviewithliked;
+  };
+
+  calcPages = moviewithliked => {
+    let copyofmovies = [...moviewithliked];
+    let pagecounter = 0;
+    let arraywithmovies = [];
+    const moviesperpage = 4;
+    let arraywithpages = [];
+
+    for (let i = 0; i <= copyofmovies.length; i++) {
+      pagecounter++;
+      arraywithmovies.push(copyofmovies[i]);
+      //copyofmovies.splice(copyofmovies[i], 1);
+      if (pagecounter === moviesperpage) {
+        arraywithpages.push(arraywithmovies);
+        pagecounter = 0;
+        arraywithmovies = [];
+        copyofmovies.splice(copyofmovies[i], 4);
+        i = -1;
+      }
+    }
   };
 
   state = {
     movies: this.fetchMovies(),
-    favmovies: {}
+    pages: {}
   };
 
   handleDelete = id => {
@@ -27,30 +51,58 @@ class movies extends Component {
   fillHeart = id => {
     const movies = [...this.state.movies];
     const movie = movies.filter(obj => obj._id === id);
-    this.addtoFavs(movie);
+    //this.addtoFavs(movie);
     if (movie.map(obj => obj.liked) == "fa fa-heart-o") {
       movie.map(obj => (obj.liked = "fa fa-heart"));
-      this.setState({ movie });
+      this.setState({ movies });
     } else {
       movie.map(obj => (obj.liked = "fa fa-heart-o"));
-      this.setState({ movie });
+      this.setState({ movies });
     }
   };
 
   addtoFavs = movie => {
-    if (
+    /* if (
       Object.entries(this.state.favmovies).length === 0 &&
       this.state.favmovies.constructor === Object
     ) {
       let favmovies = movie;
-      this.setState(favmovies);
-    } else {
-      const favmovies = [...this.state.favmovies];
-      let favmovie = favmovies.filter(obj => obj.liked == "fa fa-heart");
-      console.log(favmovie);
-      favmovies.push(favmovie);
       this.setState({ favmovies });
     }
+
+    let counter;
+
+    if (counter === null) {
+      let favmovies = movie;
+      this.setState({ favmovies: movie });
+      counter++;
+    } else {
+      let pass;
+
+      for (let key in this.state.favmovies) {
+        console.log(key, this.state.favmovies[key]);
+      }
+
+       this.state.favmovies.forEach(element => {
+        if (movie.map(obj => obj._id) === element.map(obj => obj._id)) {
+          pass = false;
+        } else {
+          pass = true;
+        }
+      });
+
+      if (pass) {
+        console.log("passed");
+        const favmovies = [...this.state.favmovies];
+        favmovies.push(movie);
+        this.setState({ favmovies });
+      } else if (!pass) {
+        console.log("not passed");
+        const favmovies = [...this.state.favmovies];
+        favmovies.splice(movie);
+        this.setState({ favmovies });
+      }
+    } */
   };
 
   render() {
@@ -81,6 +133,7 @@ class movies extends Component {
               />
             ))}
           </tbody>
+          <Pagination />
         </table>
       </React.Fragment>
     );
